fd7a3dfa7b33bb53d64abafcaa265c2e8e2a9353
Harsh Mehta <hsmehta1411@gmail.com>
2020-08-13 01:08:52 -0400

    added some files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn finalmodel.wsgi
+web: gunicorn app.app
---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__)
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
